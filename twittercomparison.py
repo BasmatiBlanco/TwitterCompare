625e74a3bea6617bd1d64152b781996caf773091
BasmatiBlanco <70245438+BasmatiBlanco@users.noreply.github.com>
2020-09-02 12:59:01 -0400

    Add files via upload

--- twittercomparison.py
+++ twittercomparison.py
@@ -6,29 +6,28 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
 
-tweet_list= []
-
 
 def recent_tweet_list(screen_name):
     new_tweets = api.user_timeline (screen_name = screen_name,count=200)
+    tweet_list = []
+    tweet_fav = 0
     tweet_list.extend(new_tweets)
-    total = 0
     for tweet in tweet_list:
         if tweet.text.startswith("RT"):
             tweet_list.remove(tweet)
         elif tweet.text.startswith("@"):
             tweet_list.remove(tweet)
         else:
-            total += int(tweet.favorite_count)
-        return int(total)
+            tweet_fav += tweet.favorite_count
+    return tweet_fav
 
 def compare_tweet_favs(user1, user2):
     total_user1= recent_tweet_list(user1)
     total_user2= recent_tweet_list(user2)
     if total_user1 > total_user2:
-        print( str(user1) + " is the winner with " + str(total_user1) + "favorites")
+        print( str(user1) + " is the winner with " + str(total_user1) + " favorites compared to " + str(user2) + "'s " + str(total_user2) + " favorites")
     elif total_user1 < total_user2:
-        print( str(user2) + " is the winner with " + str(total_user2) + "favorites")
+        print( str(user2) + " is the winner with " + str(total_user2) + " favorites" + " favorites compared to " + str(user2) + "'s " + str(total_user2) + " favorites")
     elif total_user1 == total_user2:
         print("It's a tie! both " + str(user1) + " and " + str(user2) + " have the same amount of favorites")
 
